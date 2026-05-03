a839f92cbbbb16a0707622fdff8b1073529e587d
IbrahimYemi <ibrahimsharafadeen95@gmail.com>
2026-05-03 08:21:00 +0100

    feat: Implement ingestion service with Kafka integration and data simulation

---
+++ ingestion-service/src/main/java/com/ibrahimyemi/ingestion_service/controller/IngestionController.java
@@ -0,0 +1,28 @@
+package com.ibrahimyemi.ingestion_service.controller;
+
+import com.ibrahimyemi.ingestion_service.dto.EnergyUsageDto;
+import com.ibrahimyemi.ingestion_service.services.IngestionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/ingestion")
+public class IngestionController {
+
+    private final IngestionService ingestionService;
+
+    public IngestionController(IngestionService ingestionService) {
+        this.ingestionService = ingestionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> ingestData(@RequestBody EnergyUsageDto usageDto) {
+        ingestionService.ingestEnergyUsage(usageDto);
+        return ResponseEntity.ok(Map.of("message", "Message sent"));
+    }
+}
